4873c705c917e76c66f45085c1fda8f9c569a379
Serkan Demirel <serkandemirel0420@gmail.com>
2022-04-18 10:44:41 -0700

    vector iterate

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,12 @@ fn main() {
     }
 
     let mut u: Vec<Thing> = Vec::new(); // create empty vector of unsigned 8 bit int, can grow
-    u.push(Thing::Number(2)); // append item to vector    
+    u.push(Thing::Number(2)); // append item to vector
     u.push(Thing::String(String::from("Serkan"))); // append item to vector
-    println!("{:?}", u);
-   
+    // println!("{:?}", u);
+
+    let vecter_iterator = u.iter();
+    for elem in vecter_iterator {
+        println!("{:?}", elem);
+    }
 }
