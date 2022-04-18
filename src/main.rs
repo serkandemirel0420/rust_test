cedc6c0bfacb20cd4af36f85e6b2b0769d56c3c0
Serkan Demirel <serkandemirel0420@gmail.com>
2022-04-18 10:41:05 -0700

    vector with enum multiple value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello world!");
+
+    #[derive(Debug)]
+    enum Thing {
+        String(String),
+        Number(i32),
+    }
+
+    let mut u: Vec<Thing> = Vec::new(); // create empty vector of unsigned 8 bit int, can grow
+    u.push(Thing::Number(2)); // append item to vector    
+    u.push(Thing::String(String::from("Serkan"))); // append item to vector
+    println!("{:?}", u);
+   
 }
